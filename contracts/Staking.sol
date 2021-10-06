4332292b2fbe5fe2c4e224d4e9afc7ff5531d882
MrLightWood <sailekeevalikhan@gmail.com>
2021-10-06 23:23:45 +0600

    some improvements

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,17 +2,26 @@
 pragma solidity ^0.8.9;
 
 import "./Ownable.sol";
-import "./IERC20.sol";
 import "./ABDKMathQuad.sol";
-import "./ABDKMath64x64.sol";
+
+interface ERC20 {
+  function balanceOf(address owner) external view returns (uint);
+  function allowance(address owner, address spender) external view returns (uint);
+  function approve(address spender, uint value) external returns (bool);
+  function transfer(address to, uint value) external returns (bool);
+  function transferFrom(address from, address to, uint value) external returns (bool); 
+}
+
 
 contract Staking is Ownable {
     using ABDKMathQuad for *;
 
+    ERC20 public ERC20Interface = ERC20(0xb6710572A14cEa1dc95dff07a292d2fe7c223700);
+
     constructor(uint256 _supply) {
         poolBalance = ABDKMathQuad.fromUInt(toNanoToken(_supply));
         initialPoolBalance = toNanoToken(_supply);
-        dailyReward = ABDKMathQuad.mul(ABDKMathQuad.div(ABDKMathQuad.fromUInt(_supply), ABDKMathQuad.fromUInt(365*4)), ABDKMathQuad.fromUInt(10 ** decimals()));
+        dailyReward = ABDKMathQuad.mul(ABDKMathQuad.div(ABDKMathQuad.fromUInt(_supply), ABDKMathQuad.fromUInt(7)), ABDKMathQuad.fromUInt(10 ** decimals()));
     }
 
     struct Stakeholder {
@@ -36,6 +45,46 @@ contract Staking is Ownable {
 
     function toNanoToken(uint256 token) internal pure returns(uint256) {
         return token * (10 ** decimals());
+    }
+
+    function calculateReward(address _stakeholder)
+       internal
+       view
+       returns(bytes16)
+   {
+       uint256 stakesPool = totalStakes();
+       uint256 stakeHolderStake = stakeHolders[_stakeholder].stake;
+       if(stakeHolderStake == 0) return ABDKMathQuad.fromInt(0);
+       bytes16 stakePercentage = ABDKMathQuad.mul(ABDKMathQuad.div(ABDKMathQuad.fromUInt(stakeHolderStake), ABDKMathQuad.fromUInt(stakesPool)), ABDKMathQuad.fromUInt(100));
+        
+        return ABDKMathQuad.div(ABDKMathQuad.mul(dailyReward, stakePercentage) , ABDKMathQuad.fromUInt(100));
+   }
+
+    function rewardBalanceOf(address _stakeholder)
+       internal
+       view
+       returns(uint256)
+   {
+       return ABDKMathQuad.toUInt(stakeHolders[_stakeholder].rewardBalance);
+   }
+
+   function totalRewardBalance()
+       internal
+       view
+       returns(uint256)
+   {
+       bytes16 _totalRewards;
+       for (uint256 s = 0; s < stakeHoldersList.length; s += 1){
+           _totalRewards = ABDKMathQuad.add(_totalRewards, stakeHolders[stakeHoldersList[s]].rewardBalance);
+       }
+       return ABDKMathQuad.toUInt(_totalRewards);
+   }
+
+    // <================================ PUBLIC FUNCTIONS ================================>
+
+    function transferTokensToContract() public
+    {
+        ERC20Interface.transferFrom(msg.sender, address(this), initialPoolBalance);
     }
 
    function isStakeHolder(address _address) public view returns(bool, uint256) {
@@ -73,8 +122,6 @@ contract Staking is Ownable {
        return stakeHolders[_stakeholder].stake; //stakes[_stakeholder];
    }
 
-    // <================================ PUBLIC FUNCTIONS ================================>
-
     function balanceOfPool()
        public
        view
@@ -96,16 +143,11 @@ contract Staking is Ownable {
        return _totalStakes;
    }
 
-    function createStakeMod(address _address, uint256 _stake)
-       public
-   {
-        if(stakeHolders[_address].stake == 0) addStakeHolder(_address);
-        stakeHolders[_address].stake += _stake * (10 ** decimals());
-   }
-
    function createStake(uint256 _stake)
        public
    {
+        require(_stake >= 10000, "Minimal stake value is 10 000 euphoria tokens");
+        require(ABDKMathQuad.toUInt(poolBalance) > 0, "Pool is empty. System does not accept any new stakes");
         if(stakeHolders[msg.sender].stake == 0) addStakeHolder(msg.sender);
         stakeHolders[msg.sender].stake += toNanoToken(_stake);
    }
@@ -116,42 +158,11 @@ contract Staking is Ownable {
        (bool _isStakeHolder, ) = isStakeHolder(msg.sender);
        require(_isStakeHolder == true, "You are not a Stake holder yet. Please add some stakes first");
        require(stakeHolders[msg.sender].stake >= _stake, "Provided stake value is higher compared to existsing accounts's stake balance");
+       //stakeHolders[msg.sender].rewardBalance = ABDKMathQuad.add(ABDKMathQuad.fromUInt(stakeHolders[msg.sender].stake), stakeHolders[msg.sender].rewardBalance);
        stakeHolders[msg.sender].stake -= _stake;
+       ERC20Interface.transfer(msg.sender, _stake);
        if(stakeHolders[msg.sender].stake == 0 && ABDKMathQuad.toUInt(stakeHolders[msg.sender].rewardBalance) == 0) removeStakeHolder(msg.sender);
 
-   }
-
-   function rewardBalanceOf(address _stakeholder)
-       public
-       view
-       returns(uint256)
-   {
-       return ABDKMathQuad.toUInt(stakeHolders[_stakeholder].rewardBalance);
-   }
-
-   function totalRewardBalance()
-       public
-       view
-       returns(uint256)
-   {
-       bytes16 _totalRewards;
-       for (uint256 s = 0; s < stakeHoldersList.length; s += 1){
-           _totalRewards = ABDKMathQuad.add(_totalRewards, stakeHolders[stakeHoldersList[s]].rewardBalance);
-       }
-       return ABDKMathQuad.toUInt(_totalRewards);
-   }
-
-    function calculateReward(address _stakeholder)
-       public
-       view
-       returns(bytes16)
-   {
-       uint256 stakesPool = totalStakes();
-       uint256 stakeHolderStake = stakeHolders[_stakeholder].stake;
-       if(stakeHolderStake == 0) return ABDKMathQuad.fromInt(0);
-       bytes16 stakePercentage = ABDKMathQuad.mul(ABDKMathQuad.div(ABDKMathQuad.fromUInt(stakeHolderStake), ABDKMathQuad.fromUInt(stakesPool)), ABDKMathQuad.fromUInt(100));
-        
-        return ABDKMathQuad.div(ABDKMathQuad.mul(dailyReward, stakePercentage) , ABDKMathQuad.fromUInt(100));
    }
 
     function getDailyReward() public view returns(uint256) {
@@ -162,6 +173,7 @@ contract Staking is Ownable {
         public
         onlyOwner
     {
+        require(ABDKMathQuad.toUInt(poolBalance) > 0, "Pool is empty. Can't distribute rewards");
         for (uint256 s = 0; s < stakeHoldersList.length; s += 1){
             address _stakeholder = stakeHoldersList[s];
             bytes16 reward = calculateReward(_stakeholder);
@@ -174,6 +186,7 @@ contract Staking is Ownable {
                 if(totalRewards < initialPoolBalance && (initialPoolBalance-totalRewards) < ABDKMathQuad.toUInt(reward)) {
                     uint256 remainder = initialPoolBalance - totalRewards;
                     stakeHolders[_stakeholder].rewardBalance = ABDKMathQuad.add(ABDKMathQuad.fromUInt(remainder), stakeHolders[_stakeholder].rewardBalance);
+                    poolBalance = ABDKMathQuad.fromUInt(0);
                 }
                 break;
             }
@@ -185,6 +198,9 @@ contract Staking is Ownable {
     function withdrawReward()
         public
     {
+        uint256 reward = ABDKMathQuad.toUInt(stakeHolders[msg.sender].rewardBalance);
+        require(reward > 0, "User does not have any rewarded tokens in his balance");
+        ERC20Interface.transfer(msg.sender, reward);
         stakeHolders[msg.sender].rewardBalance = 0;
     }
 }
